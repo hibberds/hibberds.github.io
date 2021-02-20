b95c12c53587f9ad70e91832a19b3b3bce26a08c
Tom Ratcliffe <tom.ratcliffe@congenica.com>
2021-02-20 17:20:03 +0000

    Initial proof of concept with Next.js

---
+++ components/NavItem.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+function NavItem({ title, url }) {
+  return (
+    <Link href={url}>{title}</Link>
+  );
+}
+
+export default NavItem;
---
+++ pages/404.js
@@ -0,0 +1,9 @@
+function NotFound() {
+  return (
+    <div>
+      Sorry! We couldn't find that page...
+    </div>
+  );
+}
+
+export default NotFound;
